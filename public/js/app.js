e454daca21cceb4282405866222fd77568aa5afa
Guilherme Alves Carvalho <guilherme.092011@hotmail.com>
2020-07-15 14:51:49 -0300

    Removed unnecessary comment

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,6 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-
-/* messageOne.textContent = 'From js'; */
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
